586c029fa5481740b7a1e0f53292aeb703b60522
DESKTOP-EH5IHIH\User <batyrovshatlyk25@gmail.com>
2025-12-08 22:31:09 +0300

    While Dongusu ornek - 2

--- odev.c
+++ odev.c
@@ -1,16 +1,38 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     printf("Sayi girin:");
+//     scanf("%d", &n);
+
+//     while (n > 0)
+//     {
+//         printf("%d\n", n);
+//         n--;
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int n;
-    printf("Sayi girin:");
-    scanf("%d", &n);
+    int sayi, toplam = 0;
 
-    while (n > 0)
+    while (1)
     {
-        printf("%d\n", n);
-        n--;
+        printf("Sayi giriniz:");
+        if (scanf("%d", &sayi) == 1)
+        {
+            toplam += sayi;
+            printf("%d\n", toplam);
+        }
+        else
+        {
+            break;
+        }
     }
-
     return 0;
 }
